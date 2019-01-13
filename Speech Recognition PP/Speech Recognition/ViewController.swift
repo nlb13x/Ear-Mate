dcfaf8e83811c3bacf6c423685b2e818b8ef1712
Nipun <nipun00123@gmail.com>
2019-01-13 11:43:07 -0500

    works pretty good except | end->doesnt trigger words again  |

--- Speech Recognition PP/Speech Recognition/ViewController.swift
+++ Speech Recognition PP/Speech Recognition/ViewController.swift
@@ -9,7 +9,6 @@
 import UIKit
 import Speech
 import AudioToolbox
-import AVFoundation
 
 extension UIDevice {
     static func vibrate() {
@@ -26,13 +25,16 @@ class ViewController: UIViewController, SFSpeechRecognizerDelegate {
     @IBOutlet weak var endButton: UIButton!
     
     let audioEngine = AVAudioEngine()
-    let audioEngine1 = AVAudioEngine()
+ //   let audioEngine1 = AVAudioEngine()
     let speechRecognizer: SFSpeechRecognizer? = SFSpeechRecognizer()
-    let speechRecognizer1: SFSpeechRecognizer? = SFSpeechRecognizer()
+ //   let speechRecognizer1: SFSpeechRecognizer? = SFSpeechRecognizer()
     let request = SFSpeechAudioBufferRecognitionRequest()
     let request1 = SFSpeechAudioBufferRecognitionRequest()
     var recognitionTask: SFSpeechRecognitionTask?
-    var recognitionTask1: SFSpeechRecognitionTask?
+ //   var recognitionTask1: SFSpeechRecognitionTask?
+//    var bestString1: String = ""
+    var bestString: String = ""
+    var lastString: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -44,14 +46,69 @@ class ViewController: UIViewController, SFSpeechRecognizerDelegate {
     @IBAction func startButtonTapped(_ sender: UIButton) {
         self.recordAndRecognizeSpeech()
     }
+    
 
     @IBAction func endButtonTapped(_ sender: UIButton) {
+        self.endRecording()
+        self.detectedTextLabel.text = ""
+    //    self.bestString1 = ""
+        self.bestString = ""
     }
+    
     
     func recordAndRecognizeSpeechDefault() {
         guard let node: AVAudioInputNode = audioEngine.inputNode else { return }
         let recordingFormat = node.outputFormat(forBus: 0)
         node.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { buffer, _ in self.request.append(buffer)
+        }
+        audioEngine.prepare()
+        do {
+            try audioEngine.start()
+        } catch {
+            return print(error)
+        }
+        guard let myRecognizer = SFSpeechRecognizer() else {
+            return
+        }
+        if !myRecognizer.isAvailable {
+            return
+        }
+        recognitionTask = speechRecognizer?.recognitionTask(with: request, resultHandler: { result, error in
+            if let result = result {
+                self.bestString = result.bestTranscription.formattedString
+                self.lastString = ""
+                for segment in result.bestTranscription.segments {
+                    let indexTo = self.bestString.index(self.bestString.startIndex, offsetBy: segment.substringRange.location)
+                    self.lastString = self.bestString.substring(from: indexTo)
+                    
+                    if self.checkForTriggerWord(resultString: self.lastString) {
+                        self.endRecording()
+                        AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
+                        self.recordAndRecognizeSpeech()
+                    }
+                }
+                
+            } else if let error = error {
+                print(error)
+            }
+        })
+    }
+    
+    func endRecording() {
+        
+    self.audioEngine.stop()
+    audioEngine.inputNode.removeTap(onBus: 0)
+    request.endAudio()
+    recognitionTask?.cancel()
+    
+        
+    }
+    
+    
+    func recordAndRecognizeSpeech() {
+        guard let node: AVAudioInputNode = audioEngine.inputNode else { return }
+         let recordingFormat = node.outputFormat(forBus: 0)
+        node.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { buffer, _ in self.request1.append(buffer)
         }
         audioEngine.prepare()
         do {
@@ -67,49 +124,10 @@ class ViewController: UIViewController, SFSpeechRecognizerDelegate {
             // A recognizer is not available right now
             return
         }
-        recognitionTask = speechRecognizer?.recognitionTask(with: request, resultHandler: { result, error in
+        recognitionTask = speechRecognizer?.recognitionTask(with: request1, resultHandler: { result, error1 in
             if let result = result {
-                let bestString = result.bestTranscription.formattedString
-                
-                var lastString: String = ""
-                for segment in result.bestTranscription.segments {
-                    let indexTo = bestString.index(bestString.startIndex, offsetBy: segment.substringRange.location)
-                    lastString = bestString.substring(from: indexTo)
-                    if self.checkForTriggerWord(resultString: lastString) {
-                        self.recordAndRecognizeSpeech()
-                    }
-                }
-                
-            } else if let error = error {
-                print(error)
-            }
-        })
-    }
-    
-    
-    func recordAndRecognizeSpeech() {
-        guard let node1: AVAudioInputNode = audioEngine1.inputNode else { return }
-         let recordingFormat1 = node1.outputFormat(forBus: 0)
-        node1.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat1) { buffer, _ in self.request1.append(buffer)
-        }
-        audioEngine1.prepare()
-        do {
-            try audioEngine1.start()
-        } catch {
-            return print(error)
-        }
-        guard let myRecognizer1 = SFSpeechRecognizer() else {
-            // A recognizer is not supported for current locale
-            return
-        }
-        if !myRecognizer1.isAvailable {
-            // A recognizer is not available right now
-            return
-        }
-        recognitionTask1 = speechRecognizer1?.recognitionTask(with: request1, resultHandler: { result1, error1 in
-            if let result1 = result1 {
-                let bestString1 = result1.bestTranscription.formattedString
-                self.detectedTextLabel.text = bestString1
+                self.bestString = result.bestTranscription.formattedString
+                self.detectedTextLabel.text = self.bestString
                 
              // var lastString: String = ""
               //  for segment in result.bestTranscription.segments {
