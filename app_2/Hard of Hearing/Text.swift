ed800cfd35d6aaa1ac907f1598ca7a3e92e08de2
Nipun <nipun00123@gmail.com>
2019-01-13 14:35:26 -0500

    Nipun_changes

--- app_2/Hard of Hearing/Text.swift
+++ app_2/Hard of Hearing/Text.swift
@@ -7,15 +7,43 @@
 //
 
 import UIKit
+import Speech
+import AudioToolbox
 
 class Text: UIViewController {
 
+    
+    @IBOutlet weak var detectedTextLabel: UILabel!
+    
+    let audioEngine = AVAudioEngine()
+    //   let audioEngine1 = AVAudioEngine()
+    let speechRecognizer: SFSpeechRecognizer? = SFSpeechRecognizer()
+    //   let speechRecognizer1: SFSpeechRecognizer? = SFSpeechRecognizer()
+    let request = SFSpeechAudioBufferRecognitionRequest()
+    let request1 = SFSpeechAudioBufferRecognitionRequest()
+    var recognitionTask: SFSpeechRecognitionTask?
+    //   var recognitionTask1: SFSpeechRecognitionTask?
+    //    var bestString1: String = ""
+    var bestString: String = ""
+    var lastString: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.recordAndRecognizeSpeech()
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func startButtonTapped(_ sender: UIButton) {
+        self.recordAndRecognizeSpeech()
+    }
+    
+    
+    @IBAction func endButtonTapped(_ sender: UIButton) {
+        self.endRecording()
+        self.detectedTextLabel.text = ""
+        //    self.bestString1 = ""
+        self.bestString = ""
+    }
 
     /*
     // MARK: - Navigation
@@ -26,5 +54,47 @@ class Text: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    
+    
+    
+    
+    func recordAndRecognizeSpeech() {
+        guard let node: AVAudioInputNode = audioEngine.inputNode else { return }
+        let recordingFormat = node.outputFormat(forBus: 0)
+        node.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { buffer, _ in self.request1.append(buffer)
+        }
+        audioEngine.prepare()
+        do {
+            try audioEngine.start()
+        } catch {
+            return print(error)
+        }
+        guard let myRecognizer = SFSpeechRecognizer() else {
+            // A recognizer is not supported for current locale
+            return
+        }
+        if !myRecognizer.isAvailable {
+            // A recognizer is not available right now
+            return
+        }
+        recognitionTask = speechRecognizer?.recognitionTask(with: request1, resultHandler: { result, error1 in
+            if let result = result {
+                self.bestString = result.bestTranscription.formattedString
+                self.detectedTextLabel.text = self.bestString
+                
+                
+            } else if let error1 = error1 {
+                print(error1)
+            }
+        })
+    }
+    
+    func endRecording() {
+        self.audioEngine.stop()
+        audioEngine.inputNode.removeTap(onBus: 0)
+        request.endAudio()
+        recognitionTask?.cancel()
+    }
 
 }
--- app_2/Hard of Hearing/ViewController.swift
+++ app_2/Hard of Hearing/ViewController.swift
@@ -7,8 +7,16 @@
 //
 
 import UIKit
+import Speech
+import AudioToolbox
 
-class ViewController: UIViewController {
+extension UIDevice {
+    static func vibrate() {
+        AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+    }
+}
+
+class ViewController: UIViewController, SFSpeechRecognizerDelegate {
     
     @IBOutlet weak var left1: UIButton!
     @IBOutlet weak var left2: UIButton!
@@ -16,6 +24,18 @@ class ViewController: UIViewController {
     @IBOutlet weak var right1: UIButton!
     @IBOutlet weak var right2: UIButton!
     @IBOutlet weak var right3: UIButton!
+    
+    let audioEngine = AVAudioEngine()
+    //   let audioEngine1 = AVAudioEngine()
+    let speechRecognizer: SFSpeechRecognizer? = SFSpeechRecognizer()
+    //   let speechRecognizer1: SFSpeechRecognizer? = SFSpeechRecognizer()
+    let request = SFSpeechAudioBufferRecognitionRequest()
+    let request1 = SFSpeechAudioBufferRecognitionRequest()
+    var recognitionTask: SFSpeechRecognitionTask?
+    //   var recognitionTask1: SFSpeechRecognitionTask?
+    //    var bestString1: String = ""
+    var bestString: String = ""
+    var lastString: String = ""
     
 
     @IBAction func textButton(_ sender: UIButton) {
@@ -36,16 +56,88 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        
         //button
+        left1.layer.cornerRadius = 12
+        left2.layer.cornerRadius = 12
+        left3.layer.cornerRadius = 12
+        right1.layer.cornerRadius = 12
+        right2.layer.cornerRadius = 12
+        right3.layer.cornerRadius = 12
         
-        
-        
-        
+        self.recordAndRecognizeSpeechDefault()
         
         
     }
+    
+    func recordAndRecognizeSpeechDefault() {
+        guard let node: AVAudioInputNode = audioEngine.inputNode else { return }
+        let recordingFormat = node.outputFormat(forBus: 0)
+        node.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { buffer, _ in self.request.append(buffer)
+        }
+        audioEngine.prepare()
+        do {
+            try audioEngine.start()
+        } catch {
+            return print(error)
+        }
+        guard let myRecognizer = SFSpeechRecognizer() else {
+            return
+        }
+        if !myRecognizer.isAvailable {
+            return
+        }
+        recognitionTask = speechRecognizer?.recognitionTask(with: request, resultHandler: { result, error in
+            if let result = result {
+                self.bestString = result.bestTranscription.formattedString
+                self.lastString = ""
+                for segment in result.bestTranscription.segments {
+                    let indexTo = self.bestString.index(self.bestString.startIndex, offsetBy: segment.substringRange.location)
+                    self.lastString = self.bestString.substring(from: indexTo)
+                    if self.checkForTriggerWord(resultString: self.lastString) {
+                        self.endRecording()
+                    AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
+                        self.performSegue(withIdentifier: "segue_text", sender: self)
+                        
+                    }
+                }
+                
+            } else if let error = error {
+                print(error)
+            }
+        })
+    }
+    
+    func endRecording() {
+        
+        self.audioEngine.stop()
+        audioEngine.inputNode.removeTap(onBus: 0)
+        request.endAudio()
+        recognitionTask?.cancel()
+        
+    }
 
+    
+    func checkForTriggerWord(resultString: String) ->Bool {
+        switch resultString {
+        case "Hello":
+            return true
+        case "Hey":
+            return true
+        case "hey":
+            return true
+        case "hello":
+            return true
+        case "John":
+            return true
+        case "john":
+            return true
+        case "hi":
+            return true
+        case "Hi":
+            return true
+        default: return false
+        }
+    }
 
 }
 
