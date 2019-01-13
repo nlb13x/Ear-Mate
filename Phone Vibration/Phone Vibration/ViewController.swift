2491c06dbe0fd8660055d54673cf0a29073d80a4
Nipun Lamba <31090991+nlb13x@users.noreply.github.com>
2019-01-13 01:01:38 -0500

    Phone Vibration

---
+++ Phone Vibration/Phone Vibration/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Phone Vibration
+//
+//  Created by Mazui san on 2019-01-12.
+//  Copyright © 2019 Mazui san. All rights reserved.
+//
+
+import UIKit
+import AudioToolbox
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func changeBackgroundColourToBlack(_ sender: Any) {
+        //view.backgroundColor = UIColor.black
+    AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
+        
+        
+        
+    }
+    
+}
+
